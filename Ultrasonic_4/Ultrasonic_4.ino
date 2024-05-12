bdd02d69916df678d010ab1eaad520820e0d1fcb
Lee-Yongsu <lysu3612@gmail.com>
2024-05-12 16:49:36 +0900

    Change to bit masking, Add count

--- Ultrasonic_4/Ultrasonic_4.ino
+++ Ultrasonic_4/Ultrasonic_4.ino
@@ -1,102 +1,145 @@
-#define pinTrig_1     3
-#define pinEcho_1     2
+// 차량 전장, 전폭 설정
+// 초음파 센서 Trig, Echo 핀 번호 설정
 
-#define pinTrig_2     6
-#define pinEcho_2     5
+#define pinTrig_front     0x08
+#define pinEcho_front     0x04
+#define pinTrig_back     0x40
+#define pinEcho_back     0x20
+#define pinTrig_left     0x02
+#define pinEcho_left     0x01
+#define pinTrig_right     0x10
+#define pinEcho_right     0x08
 
-#define pinTrig_3     9
-#define pinEcho_3     8
+#define Car_Length    30
+#define Car_Width     10
 
-#define pinTrig_4     12
-#define pinEcho_4     11
-
-double measureDistanceCm_1() {
-  digitalWrite(pinTrig_1, LOW);
+// 전방 거리 측정
+double distanceCm_front() {
+  PORTD &= ~pinTrig_front;
   delayMicroseconds(5);
-  digitalWrite(pinTrig_1, HIGH);
+  
+  PORTD |= pinTrig_front;
   delayMicroseconds(10);
-  digitalWrite(pinTrig_1, LOW);
 
-  double duration = pulseIn(pinEcho_1, HIGH);
+  PORTD &= ~pinTrig_front;
+
+  double duration = pulseIn(2, HIGH);
 
   double cm = (duration/2) * 0.0343;
   return cm;
 }
 
-double measureDistanceCm_2() {
-  digitalWrite(pinTrig_2, LOW);
+// 후방 거리 측정
+double distanceCm_back() {
+  PORTD &= ~pinTrig_back;
   delayMicroseconds(5);
-  digitalWrite(pinTrig_2, HIGH);
+  
+  PORTD |= pinTrig_back;
   delayMicroseconds(10);
-  digitalWrite(pinTrig_2, LOW);
 
-  double duration = pulseIn(pinEcho_2, HIGH);
+  PORTD &= ~pinTrig_back;
+
+  double duration = pulseIn(5, HIGH);
 
   double cm = (duration/2) * 0.0343;
   return cm;
 }
 
-double measureDistanceCm_3() {
-  digitalWrite(pinTrig_3, LOW);
+// 좌측방 거리 측정
+double distanceCm_left() {
+  PORTB &= ~pinTrig_left;
   delayMicroseconds(5);
-  digitalWrite(pinTrig_3, HIGH);
+  
+  PORTB |= pinTrig_left;
   delayMicroseconds(10);
-  digitalWrite(pinTrig_3, LOW);
 
-  double duration = pulseIn(pinEcho_3, HIGH);
+  PORTB &= ~pinTrig_left;
+
+  double duration = pulseIn(8, HIGH);
 
   double cm = (duration/2) * 0.0343;
   return cm;
 }
 
-double measureDistanceCm_4() {
-  digitalWrite(pinTrig_4, LOW);
+// 우측방 거리 측정
+double distanceCm_right() {
+  PORTB &= ~pinTrig_right;
   delayMicroseconds(5);
-  digitalWrite(pinTrig_4, HIGH);
+  
+  PORTB |= pinTrig_right;
   delayMicroseconds(10);
-  digitalWrite(pinTrig_4, LOW);
 
-  double duration = pulseIn(pinEcho_4, HIGH);
+  PORTB &= ~pinTrig_right;
+
+  double duration = pulseIn(11, HIGH);
 
   double cm = (duration/2) * 0.0343;
   return cm;
 }
 
+// Serial moritor 에 거리, 횟수 표시
+void showDistance(double d_1, double d_2, double d_3, double d_4, uint8_t c_left, uint8_t c_right) {
+  Serial.print("front: ");
+  Serial.print(d_1);
+  Serial.print("   ");
+  Serial.print("back: ");
+  Serial.print(d_2);
+  Serial.print("   ");
+  Serial.print("left: ");
+  Serial.print(d_3);
+  Serial.print("   ");
+  Serial.print("right: ");
+  Serial.println(d_4);
+
+  Serial.print("count_left: ");
+  Serial.print(c_left);
+  Serial.print("   ");
+  Serial.print("count_right: ");
+  Serial.println(c_right);
+
+  Serial.println();
+}
+
+// 횟수 카운트
+uint8_t calculateWidth(uint8_t length, uint8_t cnt) {
+  if(length > Car_Length) {
+    cnt ++ ;
+  }
+  else if(length < Car_Length) {
+    cnt = 0;
+  }
+
+  return cnt;
+}
+
+// Trig는 출력으로, Echo는 입력으로 설정
 void setup() {
   Serial.begin(9600);
 
-  pinMode(pinTrig_1, OUTPUT);
-  pinMode(pinEcho_1, INPUT);
+  DDRD |= pinTrig_front;
+  DDRD |= pinTrig_back;
+  DDRB |= pinTrig_left;
+  DDRB |= pinTrig_right;
 
-  pinMode(pinTrig_2, OUTPUT);
-  pinMode(pinEcho_2, INPUT);
-
-  pinMode(pinTrig_3, OUTPUT);
-  pinMode(pinEcho_3, INPUT);
-
-  pinMode(pinTrig_4, OUTPUT);
-  pinMode(pinEcho_4, INPUT);
+  DDRD &= ~pinEcho_front;
+  DDRD &= ~pinEcho_back;
+  DDRB &= ~pinEcho_left;
+  DDRB &= ~pinEcho_right;
 }
 
 void loop() {
-  double distance_1 = measureDistanceCm_1();
-  double distance_2 = measureDistanceCm_2();
-  double distance_3 = measureDistanceCm_3();
-  double distance_4 = measureDistanceCm_4();
-  Serial.print("1st: ");
-  Serial.print(distance_1);
-  Serial.print("   ");
+  static uint8_t count_left = 0;
+  static uint8_t count_right = 0;
 
-  Serial.print("2nd: ");
-  Serial.print(distance_2);
-  Serial.print("   ");
+  double distance_front = distanceCm_front();
+  double distance_back = distanceCm_back();
+  double distance_left = distanceCm_left();
+  double distance_right = distanceCm_right();
 
-  Serial.print("3rd: ");
-  Serial.print(distance_3);
-  Serial.print("   ");
+  count_left = calculateWidth(distance_left, count_left);
+  count_right = calculateWidth(distance_right, count_right);
 
-  Serial.print("4th: ");
-  Serial.println(distance_4);
+  showDistance(distance_front, distance_back, distance_left, distance_right, count_left, count_right);
 
   delay(1000);
 }
