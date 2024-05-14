d6b600867d07677fea0b989312b846588d805ed2
bh-1ee <pch_0063@naver.com>
2024-05-14 21:56:18 +0900

    I2C 통신 master 쓰기 성공

---
+++ I2C_Slave_write_test/I2C_Slave_write_test.ino
@@ -0,0 +1,56 @@
+uint8_t slave1 = 0x02; // 0b 00000010
+
+void slave_setup();
+void slave_read_address();
+void slave_read_data(uint8_t* p_data);
+void slave_write_address();
+void slave_write_data(uint8_t data);
+
+
+void setup() {
+  Serial.begin(9600);
+  slave_setup();
+}
+
+void loop() {
+  uint8_t data = 99; //dummy data
+  slave_read_address();
+  slave_read_data(&data);
+  Serial.print("data: ");
+  Serial.println(data);
+  delay(3000);
+}
+
+void slave_setup() {
+  TWAR = slave1; // Slave 주소 설정
+  TWCR |= (1 << TWEA) | (1 << TWEN); // TWEA 및 TWI Enable
+}
+
+void slave_read_address() {
+  TWCR |= (1 << TWINT); // 통신시작
+  while((TWSR & 0xF8) != 0x60){ // TWSR에 0x60 입력될 때까지 기다림
+    TWCR |= (1 << TWINT); // 통신시작
+    while (!(TWCR & (1 << TWINT))); // TWINT가 다시 1 될때까지 기다림
+  };
+}
+
+void slave_read_data(uint8_t* p_data) {
+  TWCR |= (1 << TWINT); // 통신 시작
+  while (!(TWCR & (1 << TWINT))); // TWINT가 다시 1 될때까지 기다림
+  while ((TWSR & 0xF8) != 0x80); // TWSR에 0x80 입력될 때까지 기다림
+  *p_data = TWDR; // data에 TWDR값 입력
+}
+
+void slave_write_address() {
+  while((TWSR & 0xF8) != 0xA8){ // TWSR에 0xA8 입력될 때까지 기다림
+    TWCR |= (1 << TWINT); // 통신시작
+    while (!(TWCR & (1 << TWINT))); // TWINT가 다시 1 될때까지 기다림
+  }
+}
+
+void slave_write_data(uint8_t data) {
+  TWDR = data; // TWDR에 data값 입력
+  TWCR |= (1 << TWINT); // 통신 시작
+  while (!(TWCR & (1 << TWINT))); // TWINT가 다시 1 될때까지 기다림
+  while ((TWSR & 0xF8) != 0xC0); // TWSR에 0xC0 입력될 때까지 기다림
+}
