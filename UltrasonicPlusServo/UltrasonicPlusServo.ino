d33152003741f0520c0bc1e48cfeb8b1f1d573d7
Lee-Yongsu <lysu3612@gmail.com>
2024-05-15 17:52:21 +0900

    deleted unuse code and annotation

--- UltrasonicPlusServo/UltrasonicPlusServo.ino
+++ UltrasonicPlusServo/UltrasonicPlusServo.ino
@@ -30,7 +30,6 @@ void setup() {
   DDRB |= REAR_SERVO;
   
   Serial.begin(9600);
-  // light_sensor_servo(); // 조도센서 기본설정 셋업
 
   front_motor0(); // 처음엔 조향각 0도
   rear_motor0();
@@ -50,12 +49,6 @@ void loop() {
   double distance_left = distanceCm_left();
   double distance_right = distanceCm_right();
 
-  // uint16_t* distance = detectCm_4direction();
-
-  // Serial.println(distance[3]);
-
-  // distance = detectCm_4direction();
-
   count_left = countLength(distance_left, count_left);
   count_right = countLength(distance_right, count_right);
 
@@ -69,53 +62,29 @@ void loop() {
 
   bool decision = decideParking(lengthmm_left);
 
-  ///////////////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////////////////////////////////////////////////////////////////////////
-
-  //front_motor0();
-  //front_motor90();
-  //front_motor180();
-  // light_sensor_servo_loop(); //ADC 돌려서 값 보기
-  // delay(500); // 텀은 0.5초 정도
   if (decision == 1){ 
     delay(2000);               // 2초 있다가 앞바퀴 2개 완전히 돌리기
     front_motor180();
     rear_motor180();
     delay(5000);
 
-    // uint16_t* update = detectCm_4direction();
     double update_distance = distanceCm_left();
-    Serial.print("update_distance: ");
-    Serial.println(update_distance);
     while(update_distance > 5){
-      // uint16_t* new_update = detectCm_4direction();
-      // update = detectCm_4direction();
       double new_update_distance = distanceCm_left();
       Serial.print("new_update_distance: ");
       Serial.println(new_update_distance);
-      // light_sensor_servo_loop();   // 계속해서 그 조건이 유지되면 바퀴 완전히 돌린거 계속 쭉 유지한다
       if (new_update_distance < 5){           // 만약에 그 조건이 유지되지 못하면 value를 초기화 함으로써 와일문을 탈출함
         count_left = 0;
-        // count_right = 0;
         break;                 
       }
     }
     front_motor0();         // 탈출 후 0도로 만들고 내가 관찰하는 value 값이 저 조건 만족안하면 계속 0도 유지
     rear_motor0();
   }
-  // else if (decision == 1){    // 2초 후 앞바퀴 완전히 돌리고 DC모터가 동작할 시간 2.5초 주고 다시 0도 만들기
-  //   delay(2000);
-  //   front_motor180();
-  //   rear_motor180();
-  //   delay(2500);
-  //   front_motor0(); // 이때 0도 만들고 내가 관찰하는 값이 저 조건 아니면 계속 0도 유지
-  //   rear_motor0();
-  // }
-  // else {
-  //   front_motor0(); // 아무 조건도 만족못하면 그냥 0도 유지
-  //   rear_motor0();
-  // }
+  else {
+    front_motor0(); // 아무 조건도 만족못하면 그냥 0도 유지
+    rear_motor0();
+  }
 }
 
 void front_motor0() { // 서보모터 0도                     
@@ -171,25 +140,6 @@ void rear_motor180() { // 서보모터 180도
     delayMicroseconds(17500);
   };
 }
-
-// void light_sensor_servo() {
-//   ADMUX |= (0 << REFS1) | (1 << REFS0); // 기준전압으로 
-//   ADMUX |= (0 << ADLAR); // 오른 정렬
-//   ADMUX |= (0 << MUX3) | (0 << MUX2) | (0 << MUX1) | (0 << MUX0); // ADC[0]자리를 사용
-//   ADCSRA |= (1 << ADEN); //ADC 가능
-//   ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); //prescaler 128 
-// }
-
-// void light_sensor_servo_loop() {
-//   while(ADCSRA & (1 << ADSC)); //ADC끝날때까지 기다리기
-//   ADCSRA |= (1 << ADSC); // ADC시작
-//   value = ADC;
-//   Serial.println(value);
-// }
-
-///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
 
 // 초음파 센서 핀 초기 설정
 void ultraSonic_setup() {
@@ -269,19 +219,6 @@ double distanceCm_right() {
   return cm;
 }
 
-// uint16_t* detectCm_4direction() {
-//   double distance_front = distanceCm_front();
-//   double distance_back = distanceCm_back();
-//   double distance_left = distanceCm_left();
-//   double distance_right = distanceCm_right();
-
-//   Serial.println(distance_right);
-
-//   static uint16_t distance_array[4] = {distance_front, distance_back, distance_left, distance_right};
-
-//   return distance_array;
-// }
-
 // Serial moritor 에 거리, 횟수, 폭 표시
 void showDistance(double d_1, double d_2, double d_3, double d_4, uint8_t c_left, uint8_t c_right, uint16_t l_left, uint16_t l_right) {
   Serial.print("front: ");
@@ -318,19 +255,11 @@ void showDistance(double d_1, double d_2, double d_3, double d_4, uint8_t c_left
 uint8_t countLength(uint8_t width, uint8_t cnt) {
   if(width > Car_Width + 2) {
     cnt += 1;
-
-    // if(cnt * Time_Interval * Velocity_MeterperSecond > Car_Width * 1000) {
-    // return cnt * Time_Interval * Velocity_MeterperSecond;
-    // }
   }
   else if(width < Car_Width + 2) {
     cnt = 0;
-
-    // return 0;
   }
-  // width = cnt * Time_Interval * Velocity_MeterperSecond;
   return cnt;
-  // return width;
 }
 
 bool decideParking(uint16_t length) {
@@ -341,7 +270,3 @@ bool decideParking(uint16_t length) {
     return 0;
   }
 }
-
-//개념 생각한것
-//일단 초음파센서로 부터 받아오는값 10번이상(차 길이 이상, 평행주차 가능)찍히면, 차폭만큼만 일단 서보모터 돌아가게 하고 dc모터 돌어갈 시간을 준다.
-// 초음파 센서로부터 읽히는 그 값 자체도 받아와서 주차를 한 후에 부족함이 있으면 그 값을 실시간으로 계속 보면서 서보모터 바퀴각을 180도 풀지 말지 결정한다. 
